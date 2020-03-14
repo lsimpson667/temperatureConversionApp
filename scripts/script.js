433e31ef029bd6acfdda22a58a2b527c6bbfadc3
Lois Simpson <lsimpson667@gmail.com>
2020-03-13 21:00:53 -0400

    added a second function to my app

--- scripts/script.js
+++ scripts/script.js
@@ -2,21 +2,32 @@
 // Declared degFahren variable with the value of the user's input
 let degFahren = prompt("Enter the degrees in Fahrenheit");
 // Declared degCel variable
-// let degCel;
+
 // create function degCel with parameter of degFahren
 function degCel(degFahren) {
    return (5 / 9) * (degFahren - 32);
 }
-// store the degCel function with the degCel argument into a new variable named "processedDegCel"
+// // store the degCel function with the degCel argument into a new variable named "processedDegCel"
 processedDegCel = degCel(degFahren);
 
-let msg = `${degFahren}\xB0 Fahrenheit is ${processedDegCel}\xB0C`;      
-if (degFahren < 69) {
-  alert(`${msg}\n Oh that's cold!`);
-} else if (degFahren > 69 && degFahren < 83) {
-  alert(`${msg}\n Temperature is just right!`);
-} else {
-  alert(`${msg}\n Its way too hot!`);
+   
+// create a function for the if statement with a parameter of degFahren that shows a message
+function showAMessage() {
+// declared the msg variable and assigned it to a string.
+    let msg = `${degFahren}\xB0 Fahrenheit is ${processedDegCel}\xB0C.`;  
+
+
+    if (degFahren < 69) {
+        alert(`${msg}\n Oh that's cold!`);
+      } else if (degFahren > 69 && degFahren < 83) {
+        alert(`${msg}\n Temperature is just right!`);
+      } else {
+        alert(`${msg}\n Its way too hot!`);
+      }
 }
+showAMessage();
+
+
+
 
 
